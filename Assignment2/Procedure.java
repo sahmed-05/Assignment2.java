b95de65bcb5f916780fb4fadb6b9136d7aa09297
Shahd Ahmed <sahmed96@montgomerycollege.edu>
2025-03-03 01:07:28 -0500

    Procedure.java

--- Assignment2/Procedure.java
+++ Assignment2/Procedure.java
@@ -1,92 +1,80 @@
 /*
  * Class: CMSC203 
- * Instructor: Khandan Monshi
- * Description: The Procedure class represents a medical procedure performed on a patient.
- * Due: 10/08/2024
- * Platform/compiler: Eclipse Java
- * I pledge that I have completed the programming 
- * assignment independently. I have not copied the code 
- * from a student or any source. I have not given my code 
- * to any student.
-   Print your Name here: Shahd Ahmed
-*/
-
-package Assignment2;
-
-/*
- * The Procedure class represents a medical procedure performed on a patient.
+ * Instructor: Grigoriy Grinberg
+ * Description: Write a class named Patient that has fields for the following data:
+•	First name, middle name, and last name
+•	Street Address, city, state, and ZIP code
+•	Phone number (example 301-123-4567)
+•	Name and phone number of emergency contact
+ * Due: 03/03/2025
+ * Platform/compiler: Eclipse
+ * I pledge that I have completed the programming assignment independently. I have not copied the code from any source.
+ * Print your Name here: Shahd Ahmed
  */
 public class Procedure {
+
     private String procedureName;
     private String procedureDate;
-    private String practitioner;
+    private String practitionerName;
     private double charge;
-
-    /*
-     * No-arg constructor that initializes a new Procedure object.
-     */
+    
+    // No-argument constructor
     public Procedure() {
+        procedureName = "";
+        procedureDate = "";
+        practitionerName = "";
+        charge = 0.0;
     }
-    /*
-     * Constructor that initializes the procedure's name and date.
-     */
+    
     public Procedure(String procedureName, String procedureDate) {
         this.procedureName = procedureName;
         this.procedureDate = procedureDate;
+        practitionerName = "";
+        charge = 0.0;
     }
-
-    /*
-     * Constructor that initializes all attributes of the procedure.
-     */
-    public Procedure(String procedureName, String procedureDate, String practitioner, double charge) {
+    
+    public Procedure(String procedureName, String procedureDate, String practitionerName, double charge) {
         this.procedureName = procedureName;
         this.procedureDate = procedureDate;
-        this.practitioner = practitioner;
+        this.practitionerName = practitionerName;
         this.charge = charge;
     }
-
-    /*
-     * Accessor and Mutator methods for each attribute
-     */
+    
+    // Accessors and mutators
     public String getProcedureName() {
         return procedureName;
     }
-
     public void setProcedureName(String procedureName) {
         this.procedureName = procedureName;
     }
-
+    
     public String getProcedureDate() {
         return procedureDate;
     }
-
     public void setProcedureDate(String procedureDate) {
         this.procedureDate = procedureDate;
     }
-
-    public String getPractitioner() {
-        return practitioner;
+    
+    public String getPractitionerName() {
+        return practitionerName;
     }
-
-    public void setPractitioner(String practitioner) {
-        this.practitioner = practitioner;
+    public void setPractitionerName(String practitionerName) {
+        this.practitionerName = practitionerName;
     }
-
+    
     public double getCharge() {
         return charge;
     }
-
     public void setCharge(double charge) {
         this.charge = charge;
     }
+    
 
-    /*
-     * Returns a string representation of the procedure's information.
-     */
     public String toString() {
-        return "          Procedure: " + procedureName + " \n" +
-               "          ProcedureDate=" + procedureDate + " \n" +
-               "          Practitioner=" + practitioner + " \n" +
-               "          Charge=" + charge + "\n";
+        String output = "\tProcedure: " + procedureName + "\n";
+        output += "\tProcedureDate=" + procedureDate + "\n";
+        output += "\tPractitioner=" + practitionerName + "\n";
+        output += "\tCharge=" + charge + "\n";
+        return output;
     }
 }
