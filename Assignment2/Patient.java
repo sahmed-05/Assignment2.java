9fdb148c140435fd7fde0cc2dde2e74cd11bc105
Shahd Ahmed <sahmed96@montgomerycollege.edu>
2025-03-03 01:06:48 -0500

    Patient.java

--- Assignment2/Patient.java
+++ Assignment2/Patient.java
@@ -1,176 +1,153 @@
 /*
  * Class: CMSC203 
- * Instructor: Khandan Monshi
- * Description: The Patient class represents each of the patients with their personal and contact information.
- * Due: 10/08/2024
- * Platform/compiler: Eclipse Java
- * I pledge that I have completed the programming 
- * assignment independently. I have not copied the code 
- * from a student or any source. I have not given my code 
- * to any student.
-   Print your Name here: Shahd Ahmed
-*/
-
-package Assignment2;
-
-/*
- * The Patient class represents each of the patients with their personal and contact information.
+ * Instructor: Grigoriy Grinberg
+ * Description: Write a class named Patient that has fields for the following data:
+•	First name, middle name, and last name
+•	Street Address, city, state, and ZIP code
+•	Phone number (example 301-123-4567)
+•	Name and phone number of emergency contact
+ * Due: 03/03/2025
+ * Platform/compiler: Eclipse
+ * I pledge that I have completed the programming assignment independently. I have not copied the code from any source.
+ * Print your Name here: Shahd Ahmed
  */
 public class Patient {
-    private String firstName;
-    private String middleName;
-    private String lastName;
-    private String address;
-    private String city;
-    private String state;
-    private String zip;
-    private String phoneNumber;
-    private String emergencyName;
-    private String emergencyPhone;
 
-    /*
-     * No-arg constructor that initializes a new Patient object.
-     */
+    private String firstName, middleName, lastName;
+    private String streetAddress, city, state, zipCode;
+    private String phoneNumber;
+    private String emergencyContactName, emergencyContactPhone;
+    
+    // No-argument constructor
     public Patient() {
+        firstName = "";
+        middleName = "";
+        lastName = "";
+        streetAddress = "";
+        city = "";
+        state = "";
+        zipCode = "";
+        phoneNumber = "";
+        emergencyContactName = "";
+        emergencyContactPhone = "";
     }
-    /*
-     * Constructor that initializes the patient's first, middle, and last name.
-     */
+    
     public Patient(String firstName, String middleName, String lastName) {
         this.firstName = firstName;
         this.middleName = middleName;
         this.lastName = lastName;
+        streetAddress = "";
+        city = "";
+        state = "";
+        zipCode = "";
+        phoneNumber = "";
+        emergencyContactName = "";
+        emergencyContactPhone = "";
     }
-
-    /*
-     * Constructor that initializes all attributes of the patient.
-     */
-    public Patient(String firstName, String middleName, String lastName, String address,
-                   String city, String state, String zip, String phoneNumber,
-                   String emergencyName, String emergencyPhone) {
+    
+    public Patient(String firstName, String middleName, String lastName, String streetAddress, 
+                   String city, String state, String zipCode, String phoneNumber, 
+                   String emergencyContactName, String emergencyContactPhone) {
         this.firstName = firstName;
         this.middleName = middleName;
         this.lastName = lastName;
-        this.address = address;
+        this.streetAddress = streetAddress;
         this.city = city;
         this.state = state;
-        this.zip = zip;
+        this.zipCode = zipCode;
         this.phoneNumber = phoneNumber;
-        this.emergencyName = emergencyName;
-        this.emergencyPhone = emergencyPhone;
+        this.emergencyContactName = emergencyContactName;
+        this.emergencyContactPhone = emergencyContactPhone;
     }
-
-    /*
-     * Accessor and Mutator methods for each attribute
-     */
+    
+    // Accessor and mutator methods for each attribute
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
+    
     public String getMiddleName() {
         return middleName;
     }
-
     public void setMiddleName(String middleName) {
         this.middleName = middleName;
     }
-
+    
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
-    public String getAddress() {
-        return address;
+    
+    public String getStreetAddress() {
+        return streetAddress;
     }
-
-    public void setAddress(String address) {
-        this.address = address;
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
     }
-
+    
     public String getCity() {
         return city;
     }
-
     public void setCity(String city) {
         this.city = city;
     }
-
+    
     public String getState() {
         return state;
     }
-
     public void setState(String state) {
         this.state = state;
     }
-
-    public String getZip() {
-        return zip;
+    
+    public String getZipCode() {
+        return zipCode;
     }
-
-    public void setZip(String zip) {
-        this.zip = zip;
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
     }
-
+    
     public String getPhoneNumber() {
         return phoneNumber;
     }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
-    public String getEmergencyName() {
-        return emergencyName;
+    
+    public String getEmergencyContactName() {
+        return emergencyContactName;
     }
-
-    public void setEmergencyName(String emergencyName) {
-        this.emergencyName = emergencyName;
+    public void setEmergencyContactName(String emergencyContactName) {
+        this.emergencyContactName = emergencyContactName;
     }
-
-    public String getEmergencyPhone() {
-        return emergencyPhone;
+    
+    public String getEmergencyContactPhone() {
+        return emergencyContactPhone;
     }
-
-    public void setEmergencyPhone(String emergencyPhone) {
-        this.emergencyPhone = emergencyPhone;
+    public void setEmergencyContactPhone(String emergencyContactPhone) {
+        this.emergencyContactPhone = emergencyContactPhone;
     }
-
-    /*
-     * Builds and returns the full name of the patient.
-     */
+    
     public String buildFullName() {
         return firstName + " " + middleName + " " + lastName;
     }
-
-    /*
-     * Builds and returns the full address of the patient.
-     */
+    
     public String buildAddress() {
-        return address + " " + city + " " + state + " " + zip;
+        return streetAddress + " " + city + " " + state + " " + zipCode;
     }
-
-    /*
-     * Builds and returns the emergency contact information.
-     */
+    
     public String buildEmergencyContact() {
-        return emergencyName + " " + emergencyPhone;
+        return emergencyContactName + " " + emergencyContactPhone;
     }
-
-    /*
-     * Returns a string representation of the patient's information.
-     */
+    
     public String toString() {
-        return "Patient info:\n" +
-               "   Name: " + buildFullName() + "\n" +
-               "   Address: " + buildAddress() + "\n" +
-               "   EmergencyContact: " + buildEmergencyContact() + "\n";
+        String output = "Patient info:\n";
+        output += "Name: " + buildFullName() + "\n";
+        output += "Address: " + buildAddress() + "\n";
+        output += "EmergencyContact: " + buildEmergencyContact();
+        return output;
     }
 }
